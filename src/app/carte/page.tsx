e0c1c735a22ae1c4e63c4c43e6912c9bd40bf57c
Sacha Guignard <sacha.guignard@caensup.fr>
2024-05-28 00:03:58 +0200

    probléme git ajout manuel du code de valentin + fix des erreur pour le deploy

--- src/app/carte/page.tsx
+++ src/app/carte/page.tsx
@@ -1,46 +1,11 @@
 'use client';
-import React, {useEffect, useState} from "react";
-import {MapContainer, Marker, Popup, TileLayer, useMap} from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import {icon} from "leaflet";
-import HttpService from "@/services/HttpService";
-import Link from "next/link";
-import {API_URLs} from "@/services/API_URLs";
+import React, {useEffect, useState} from 'react';
+import dynamic from 'next/dynamic';
+import HttpService from '@/services/HttpService';
+import {API_URLs} from '@/services/API_URLs';
 
-const ICON = icon({
-    iconUrl: "utilisateur.png",
-    iconSize: [30, 32],
-});
-
-const ICONRESTAURANT = icon({
-    iconUrl: "restaurant.svg",
-    iconSize: [30, 32],
-});
-
-// @ts-ignore
-function LocateControl({center}) {
-    const map = useMap();
-
-    const handleLocateClick = () => {
-        map.setZoom(15); // Zoom sur la position de l'utilisateur en premier
-        map.setView(center); // Puis déplacez la carte vers la position de l'utilisateur
-    };
-
-    return (
-        <button
-            onClick={handleLocateClick}
-            role="button"
-            style={{
-                position: "absolute",
-                top: "10px",
-                right: "10px",
-                zIndex: 1000,
-            }}
-        >
-            <img src="/locate-fixed.svg" alt="Locate Me"/>
-        </button>
-    );
-}
+// Dynamic import for Map component
+const Map = dynamic(() => import('@/component/map/Map'), {ssr: false});
 
 function Page() {
     const [latitude, setLatitude] = useState(0);
@@ -50,7 +15,7 @@ function Page() {
     const [defaultDistance, setDefaultDistance] = useState(5); // Valeur de distance par défaut
 
     useEffect(() => {
-        if (navigator.geolocation) {
+        if (typeof window !== 'undefined' && navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
                     setLatitude(position.coords.latitude);
@@ -83,92 +48,28 @@ function Page() {
                 }));
                 setRestaurants(formattedRestaurants);
             } catch (error) {
-                console.error("Error fetching data:", error);
+                console.error('Error fetching data:', error);
             }
         };
 
         fetchData();
     }, []);
-    // @ts-ignore
-    const handleDistanceChange = (event) => {
+
+    const handleDistanceChange = (event: { target: { value: React.SetStateAction<number>; }; }) => {
         setDefaultDistance(event.target.value);
     };
-    // @ts-ignore
-    const calculateDistance = (lat1, lon1, lat2, lon2) => {
-        const R = 6371; // Rayon de la terre en km
-        const dLat = deg2rad(lat2 - lat1);
-        const dLon = deg2rad(lon2 - lon1);
-        const a =
-            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        const dist = R * c; // Distance en km
-        return dist;
-    };
-    // @ts-ignore
-    const deg2rad = (deg) => {
-        return deg * (Math.PI / 180);
-    };
 
-
-    const filteredRestaurants = restaurants.filter(
-        (restaurant) =>
-            // @ts-ignore
-            calculateDistance(latitude, longitude, restaurant.latitude, restaurant.longitude) <= defaultDistance
-    );
-// @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
     return (
         <div>
             {!loading && (
-                <MapContainer
-                    center={[latitude, longitude]}
-                    zoom={13}
-                    scrollWheelZoom={false}
-                    style={{height: "600px", width: "100%"}}
-                >
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker icon={ICON} position={[latitude, longitude]}>
-                        <Popup>Moi !</Popup>
-                    </Marker>
-
-                    {filteredRestaurants.map((item) => (
-                        // @ts-ignore
-                        <Marker key={item.id} icon={ICONRESTAURANT} position={[item.latitude, item.longitude]}>
-                            <Popup>
-                                <div>
-
-                                    <h3>{item["name"]}</h3>
-                                    <p>{item["address1"]}</p>
-                                    <Link href={`/restos/${item["id"]}`}>Voir</Link>
-                                </div>
-                            </Popup>
-                        </Marker>
-                    ))}
-                    <LocateControl center={[latitude, longitude]}/>
-                </MapContainer>
-            )}
-
-            <div style={{margin: "10px 0"}}>
-                <label htmlFor="distanceRange">Distance (km):</label>
-                <input
-                    type="range"
-                    id="distanceRange"
-                    name="distanceRange"
-                    min="1"
-                    max="3000"
-                    step="100"
-                    value={defaultDistance}
-                    onChange={handleDistanceChange}
+                <Map
+                    latitude={latitude}
+                    longitude={longitude}
+                    restaurants={restaurants}
+                    defaultDistance={defaultDistance}
+                    handleDistanceChange={handleDistanceChange}
                 />
-
-                <span>{defaultDistance === 3000 ? "Toute la distance" : `${defaultDistance} km`}</span>
-            </div>
-
+            )}
         </div>
     );
 }
