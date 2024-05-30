73fe691d111db69ddbd2c183b6245f41a1d4b955
sacha Guignard <sacha.guignard@caensup.fr>
2024-05-30 23:40:47 +0000

    Update page.tsx

--- src/app/restos/page.tsx
+++ src/app/restos/page.tsx
@@ -1,27 +1,46 @@
 import HttpService from "@/services/HttpService";
 import Restaurant from "@/models/Restaurant";
 import Plat from "@/models/Plat";
-import {Table} from "antd";
+import { Table } from "antd";
 import Link from "next/link";
 import { API_URLs } from "@/services/API_URLs";
 
-export default async function Page() {
-    const data = (await
-        HttpService.get(API_URLs.restos))._embedded.restaurant;
+export async function getStaticProps() {
+    let data = [];
+
+    try {
+        const response = await HttpService.get(API_URLs.restos);
+        data = response._embedded.restaurant;
+    } catch (error) {
+        console.error('Failed to fetch restaurants:', error);
+        // Fallback data in case of an error
+        data = [];
+    }
+
+    return {
+        props: {
+            data,
+        },
+        revalidate: 10, // Re-generate the page at most once every 10 seconds
+    };
+}
+
+export default function Page({ data }) {
     const dData = data.map((item: Restaurant) => {
         return {
             key: item.id,
             name: item.name,
             address1: item.address1,
             action: <Link href={`/restos/${item.id}`}>Voir</Link>
-        }
+        };
     });
 
-    const columns = [{
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-    },
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
         {
             title: 'Address',
             dataIndex: 'address1',
@@ -42,7 +61,7 @@ export default async function Page() {
     return (
         <>
             <h2>Restaurant</h2>
-            <Table dataSource={dData} columns={columns}/> {/* Utilisation de dData comme source de données */}
+            <Table dataSource={dData} columns={columns} /> {/* Utilisation de dData comme source de données */}
         </>
     );
 }
