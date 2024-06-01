445d2c5fcbc6848de7f0f649048a77ee1a7c2394
Sacha Guignard <sacha.guignard@caensup.fr>
2024-06-02 00:20:14 +0200

    ajout de router pour /login

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,16 +1,34 @@
 'use client'
 import Header from "@/component/Header";
 import {useSession} from "next-auth/react";
+import UserInfo from "@/component/UserLog/UserInfo";
 import Logout from "@/component/UserLog/logout";
-
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 export default function Page() {
     const {data: session, status} = useSession();
     const userId = session?.user?.id;
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === 'unauthenticated') {
+            router.push("/login");
+        }
+    }, [status, router]);
+
+    if (status === 'loading') {
+        return <div>Loading...</div>;
+    }
+
+    if (!userId) {
+        return null;
+    }
 
     return (
         <div style={{minHeight: "100vh"}}>
             <Header returnText={"Compte"} returnLink={"/"}/>
+            <UserInfo userId={userId}></UserInfo>
             <Logout></Logout>
         </div>
     )
